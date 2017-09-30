330678bd2fc1db18e4eafd3bd239efbd7a4fdb5c
Dan Florio <dflo@danflorio.com>
2017-09-30 14:52:36 -0400

    add notes to all circles

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ class Circle extends Component {
     super();
     this.state = {on: false};
     this.onClick = this.onClick.bind(this);
-    this.synth = new Tone.Synth().toMaster();
   }
 
   onClick() {
     this.setState({on: true});
     this.props.onClick();
-    this.synth.triggerAttackRelease("C4", "8n");
     setTimeout(function(){ this.setState({on: false}); }.bind(this), 600);
   }
 
@@ -34,27 +32,32 @@ class Circle extends Component {
 }
 
 class Board extends Component {
-  handleClick(n) {
-    console.log(n);
+  constructor() {
+    super();
+    this.synth = new Tone.Synth().toMaster();
+  }
+  handleClick(i, n) {
+    console.log(i);
+    this.synth.triggerAttackRelease(n, "4n");
   }
 
   render() {
     return (
       <div className="Board">
-        <Circle level="1" pos="t" onClick={()=>this.handleClick(1)} />
-        <Circle level="1" pos="r" onClick={()=>this.handleClick(2)} />
-        <Circle level="1" pos="b" onClick={()=>this.handleClick(3)} />
-        <Circle level="1" pos="l" onClick={()=>this.handleClick(4)} />
+        <Circle level="1" pos="t" onClick={()=>this.handleClick(1, "D4")} />
+        <Circle level="1" pos="r" onClick={()=>this.handleClick(2, "E4")} />
+        <Circle level="1" pos="b" onClick={()=>this.handleClick(3, "F4")} />
+        <Circle level="1" pos="l" onClick={()=>this.handleClick(4, "G4")} />
 
-        <Circle level="2" pos="tl" onClick={()=>this.handleClick(5)} />
-        <Circle level="2" pos="tr" onClick={()=>this.handleClick(6)} />
-        <Circle level="2" pos="br" onClick={()=>this.handleClick(7)} />
-        <Circle level="2" pos="bl" onClick={()=>this.handleClick(8)} />
+        <Circle level="2" pos="tl" onClick={()=>this.handleClick(5, "G3")} />
+        <Circle level="2" pos="tr" onClick={()=>this.handleClick(6, "A3")} />
+        <Circle level="2" pos="br" onClick={()=>this.handleClick(7, "B3")} />
+        <Circle level="2" pos="bl" onClick={()=>this.handleClick(8, "C4")} />
 
-        <Circle level="3" pos="t" onClick={()=>this.handleClick(9)} />
-        <Circle level="3" pos="r" onClick={()=>this.handleClick(10)} />
-        <Circle level="3" pos="b" onClick={()=>this.handleClick(11)} />
-        <Circle level="3" pos="l" onClick={()=>this.handleClick(12)} />
+        <Circle level="3" pos="t" onClick={()=>this.handleClick(9, "C3")} />
+        <Circle level="3" pos="r" onClick={()=>this.handleClick(10, "D3")} />
+        <Circle level="3" pos="b" onClick={()=>this.handleClick(11, "E3")} />
+        <Circle level="3" pos="l" onClick={()=>this.handleClick(12, "F3")} />
       </div>
     );
   }
